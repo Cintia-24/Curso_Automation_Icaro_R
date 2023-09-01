4a3c2be643b2f4f74bcc0c1a7626270daf4dce01
cprado <cprado@clarika.com.ar>
2023-08-31 21:11:06 -0300

    Ejemplo de commit

---
+++ src/main/java/com/cvprado/clase_2/Dias_Semana.java
@@ -0,0 +1,13 @@
+package com.cvprado.clase_2;
+
+public class Dias_Semana {
+    public static void main(String[] args) {
+        System.out.println("Dia 1 - Lunes");
+        System.out.println("Dia 2 - Martes");
+        System.out.println("Dia 3 - Miercoles");
+        System.out.println("Dia 4 - Jueves");
+        System.out.println("Dia 5 - Viernes");
+        System.out.println("Dia 6 - Sabado");
+        System.out.println("Dia 7 - Domingo");
+    }
+}
---
+++ src/main/java/com/cvprado/clase_2/Hola_Mundo.java
@@ -0,0 +1,11 @@
+package com.cvprado.clase_2;
+
+public class Hola_Mundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!"); // Imprime un string
+        System.out.println(10); // Imprime un entero
+        System.out.println(20.56); // Imprime un decimal con punto y no coma
+        System.out.println(true); // Imprime un booleano
+        System.out.println("j"); // Impriem un caracter (char)
+    }
+}
