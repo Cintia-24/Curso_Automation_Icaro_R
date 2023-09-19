78b141346fcea7e98a2cee055b5da7aa99fefef0
Vanesa Prado <pradocintia294@gmail.com>
2023-09-19 10:05:35 -0300

    Practico 3 y 4 de la clase 3

--- src/main/java/com/cvprado/clase_3/P3_Operadores.java
+++ src/main/java/com/cvprado/clase_3/P3_Operadores.java
@@ -3,7 +3,29 @@ package com.cvprado.clase_3;
 public class P3_Operadores {
     public static void main(String[] args) {
 
-        //OPERADORES;
+        // Operadores aritmenticos
+        int suma = 10 + 10;
+        int resta = 10 - 10;
+        int division = 10 / 10;
+        int miltiplicacion = 10 * 10;
+
+        //Operadores Relacionales
+        boolean menor = 10 < 20;
+        System.out.println(menor);
+
+        boolean menorOIgual = 10 <= 10;
+        System.out.println(menorOIgual);
+
+        System.out.println("c" != "m");
+
+        //Operadores condicionales
+        System.out.println((10 < 20) && (100 > 1));
+
+        System.out.println((10 < 20) && (100 < 1));
+
+        System.out.println(!true);
+
+
 
 
     }
---
+++ src/main/java/com/cvprado/clase_3/P4_Condicionales.java
@@ -0,0 +1,55 @@
+package com.cvprado.clase_3;
+
+public class P4_Condicionales {
+    public static void main(String[] args) {
+
+        int var1 = 100;
+        int var2 =200;
+
+        if (var1 < var2) {
+            System.out.println( "Var1 es menor que var2");
+        }
+
+        char var3 = "c";
+        char var4 = "n";
+
+        if (var3 == var4);{
+            System.out.println("Son Iguales");
+        } else {
+            System.out.println("Son Distintos");
+        }
+
+        long var5 = 1000;
+
+        if (var5 < 10) {
+            System.out.println("Numero Chico");
+        } else if (var5 > 50 && var5 < 200) {
+            System.out.println("Numero Mediano");
+        } else if (var5 > 500 && var5 < 1000) {
+            System.out.println("Numero Grande");
+        } else {
+            System.out.println("Numero Muyyyy Grande");
+        }
+
+        String dia = "Domingo";
+
+        switch (dia) {
+            case "Lunes";
+                System.out.println(" Es Lunes");
+                break;
+            case "Martes";
+                System.out.println("Es Martes");
+                break;
+            case "Miercoles";
+                System.out.println("Es Miercoles");
+                break;
+            case "Jueves";
+                System.out.println("Es Jueves");
+                break;
+            case "Vernes";
+                System.out.println("Es Viernes");
+                break;
+            default:
+                System.out.println("Es Fin de Semana");
+        }
+}
