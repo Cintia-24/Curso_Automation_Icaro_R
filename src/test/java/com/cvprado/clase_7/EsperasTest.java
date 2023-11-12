ad1e6461870457a8617a272d1bb8250eee1d1f9d
Vanesa Prado <pradocintia294@gmail.com>
2023-11-11 22:21:25 -0300

    Test, esperas, aserciones

--- src/test/java/com/cvprado/clase_7/EsperasTest.java
+++ src/test/java/com/cvprado/clase_7/EsperasTest.java
@@ -1,4 +1,38 @@
 package com.cvprado.clase_7;
 
-public class EsperasTest {
+import com.cvprado.openCart.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
+public class EsperasTest extends BaseTest {
+
+    @Test
+    public void validarTitle() {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(15));
+
+        By title = By.xpath("//div[@id='finish']/h4");
+        By startBtn = By.xpath("//div[@id='start']/button");
+
+        getDriver().get("https://the-internet.herokuapp.com/dynamic_loading/1");
+
+        //WebElement titleElement = getDriver().findElement(title);
+
+        WebElement startElement = wait.until(ExpectedConditions.elementToBeClickable(startBtn));
+        startElement.click();
+
+        WebElement titleElement = wait.until(visibilityOfElementLocated(title));
+
+        Assert.assertEquals(titleElement.getText(), "Hello World!");
+
+    }
 }
--- src/test/java/com/cvprado/openCart/LoginTest.java
+++ src/test/java/com/cvprado/openCart/LoginTest.java
@@ -1,42 +1,57 @@
 package com.cvprado.openCart;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTest {
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void setup(){
-        ChromeOptions options = new ChromeOptions();
-
-        System.setProperty("webdriver,chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
-
-        options.addArguments("start-maximized");
-        options.addArguments("incognito");
-
-
-
-        options.setPageLoadTimeout(Duration.ofSeconds(60));
-
-        this.driver = new ChromeDriver(options);
-
-    }
+public class LoginTest extends BaseTest {
 
     @Test
+    public void validarLogin() {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
 
-    @AfterMethod
-    public void tearDown(){
-        if(this.driver !=null) {
-            this.driver.quit();
-        }
+        By myAccountBtn = By.xpath("//a [@title='My Account']");
+        ////a[contains(@href,'account/login')]
+        By loginBtn = By.xpath("//a[text()='Login']");
+
+        getDriver().get("https://opencart.abstracta.us/");
+
+        WebElement myAccountElement = wait.until(ExpectedConditions.elementToBeClickable(myAccountBtn));
+        myAccountElement.click();
+
+        WebElement loginBtnElement = wait.until(ExpectedConditions.elementToBeClickable(loginBtn));
+        loginBtnElement.click();
+
+        By emailInput = By.id("input-email");
+        By passwordInput = By.id("input-password");
+        By loginForm = By.xpath("//input[@class='btn btn-primary']");
+        //  //input[@type='submit' and @value='Login'] opcion mas robusta de xpath
+
+        WebElement emailInputElement = wait.until(ExpectedConditions.visibilityOfElementLocated(emailInput));
+        emailInputElement.sendKeys("testopencart@gmail.com");
+        WebElement passwordInputElement = wait.until(ExpectedConditions.visibilityOfElementLocated(passwordInput));
+        passwordInputElement.sendKeys("1234");
+        WebElement loginFormElement = wait.until(ExpectedConditions.elementToBeClickable(loginForm));
+        loginFormElement.click();
+
+        By myAccountTitle = By.xpath("//h2 [text()='My Account']");
+
+        WebElement myAccountTitleElement = wait.until(presenceOfElementLocated(myAccountTitle));
+
+        Assert.assertEquals(myAccountTitleElement.getText(),"My Account");
     }
 
 }
